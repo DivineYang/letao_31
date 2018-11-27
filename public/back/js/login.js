52805caf44e5f2746a9477097b97fff653502c8b
DivineYang <1634290107@qq.com>
2018-11-27 21:22:19 +0800

    登录校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,5 +1,5 @@
 $(function () {
-    console.log(234)
+    // console.log(234)
     $('#form').bootstrapValidator({
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
@@ -16,7 +16,10 @@ $(function () {
                         min: 2,
                         max: 6,
                         message: "用户名长度必须是2-6位"
-                    }
+                    },
+                    callback:{
+                        message:"用户名不存在",
+                    },
                 }
             },
             password: {
@@ -28,7 +31,10 @@ $(function () {
                         min: 6,
                         max: 12,
                         message: "密码长度必须是6-12位"
-                    }
+                    },
+                    callback:{
+                        message:"密码错误",
+                    },
                 }
             }
         }
@@ -47,10 +53,12 @@ $(function () {
                 console.log(info)
                 if (info.error === 1000) {
                     alert("用户名不存在");
+                    $('#form').data("bootstrapValidator").updateStatus("username","INVALID","callback")
                     return;
                 }
                 if (info.error === 1001) {
                     alert("密码错误");
+                    $('#form').data("bootstrapValidator").updateStatus("password","INVALID","callback");
                     return;
                 }
                 if (info.success) {

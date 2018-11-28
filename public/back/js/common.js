61998e6a95fc724f70167b7c454331813ab104ac
DivineYang <1634290107@qq.com>
2018-11-28 16:36:57 +0800

    公共功能的登陆

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -8,3 +8,36 @@ $(document).ajaxStop(function(){
     },1000)
 })
 
+$(function(){
+    // 左侧二级菜单切换
+    $('.category').click(function(){
+        console.log(123)
+        $(this).next().slideToggle();
+    })
+
+    // 左侧侧边栏切换
+    $('.icon_left').click(function(){
+        $('.lt_aside').toggleClass("hidemenu")
+        $('.lt_topbar').toggleClass("hidemenu")
+        $('.lt_main').toggleClass("hidemenu")
+    })
+
+    // 退出功能
+    $('.icon_right').click(function(){
+        $('#myModal').modal("show");
+    })
+
+    $('#logoutBtn').click(function(){
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            dataType:"json",
+            success:function( info ){
+                console.log(info)
+                if(info.success){
+                    location.href = "login.html";
+                }
+            }
+        })
+    })
+})
